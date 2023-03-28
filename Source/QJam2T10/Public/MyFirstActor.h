d40f1dbe247e3c8d8e8f2f1d7517aa294c7133e4
Lautaro Cabral <lautaro.cabral.dev@gmail.com>
2023-03-28 05:00:06 -0300

    New cpp class added

---
+++ Source/QJam2T10/Public/MyFirstActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyFirstActor.generated.h"
+
+UCLASS()
+class QJAM2T10_API AMyFirstActor : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* VisualMesh;
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyFirstActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
